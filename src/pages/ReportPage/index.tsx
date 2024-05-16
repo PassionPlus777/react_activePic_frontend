16a9619b23c9a7d7bfeab46fadcd952040cfc6b2
Roman Dmitro <rc.dev0401@gmail.com>
2024-05-16 03:07:46 -0700

    create reportpage

---
+++ src/pages/ReportPage/index.tsx
@@ -0,0 +1,8 @@
+import MarkerComponent from "@/components/MarkerComponent";
+import React from "react";
+
+const MarkerPage = () => {
+  return <MarkerComponent />;
+};
+
+export default MarkerPage;
