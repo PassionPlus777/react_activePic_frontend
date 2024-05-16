466bba7ea4350329636c9c0103d1b9e3d8f8b2f8
Roman Dmitro <rc.dev0401@gmail.com>
2024-05-16 03:31:00 -0700

    finish report

--- src/components/ReportComponent/index.tsx
+++ src/components/ReportComponent/index.tsx
@@ -1,12 +1,48 @@
-import { Card } from "antd";
+import { FC } from "react";
+import { Card, Typography, Row, Col } from "antd";
+
+interface StatisticCardProps {
+  title?: string;
+  value?: number;
+}
+
+const StatisticCard: FC<StatisticCardProps> = ({ title, value }) => {
+  return (
+    <div className="flex flex-col p-2 statistic-card">
+      <Typography.Title level={5} className="text-center">
+        {title}
+      </Typography.Title>
+      <p className="text-center text-5xl font-bold my-3">{value}</p>
+    </div>
+  );
+};
 
 const ReportComponent = () => {
   return (
     <Card
-      title="PLEASE ENTER THE RANGE YOU WOULD LIKE"
+      title="EVENT STATISTICS"
       bordered={false}
       className="h-full overflow-y-scroll"
-    ></Card>
+    >
+      <Typography.Title level={5}>
+        * Please contact support@activePic.com if you have any issues
+      </Typography.Title>
+
+      <Row className="mt-8">
+        <Col span={5}>
+          <StatisticCard title={"Total Participants"} value={3615} />
+        </Col>
+        <Col span={5} offset={1}>
+          <StatisticCard title={"Images Uploaded"} value={21674} />
+        </Col>
+        <Col span={5} offset={1}>
+          <StatisticCard title={"Galleries Activated"} value={3196} />
+        </Col>
+        <Col span={5} offset={1}>
+          <StatisticCard title={"Total Impressions"} value={364885} />
+        </Col>
+      </Row>
+    </Card>
   );
 };
 
